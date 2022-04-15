a41a281c594f8271c76a546cc79f4ad4efa8bb0f
RyosukeSomeya <rx.0.0618@gmail.com>
2022-04-15 21:59:23 +0900

    firebase文字入力追加、realtimeDatabaseへのメッセージ登録実装

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 
+import { pushMessage } from '../firebase';
+
 const MessageField = ({ name, setText, text }) => {
   const [isComposed, setIsComposed] = useState(false);
-  console.log({ text });
   return (
     <TextField
       fullWidth={true}
@@ -16,7 +17,7 @@ const MessageField = ({ name, setText, text }) => {
         if (text === '') return;
 
         if (e.key === 'Enter') {
-          console.log('push message');
+          pushMessage({ name: 'test user', text });
           setText('');
           e.preventDefault();
         }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJPWi1mgMcErHaLya12DVMn7mY2bEjyyc',
+  authDomain: 'react-chat-app-d75b4.firebaseapp.com',
+  databaseURL: 'https://react-chat-app-d75b4-default-rtdb.firebaseio.com',
+  projectId: 'react-chat-app-d75b4',
+  storageBucket: 'react-chat-app-d75b4.appspot.com',
+  messagingSenderId: '484640258238',
+  appId: '1:484640258238:web:32d675a8655614f41106ef',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};

ed41939c19b19237d3338a995d620aebd9526930
RyosukeSomeya <rx.0.0618@gmail.com>
2022-04-15 22:32:24 +0900

    firebaseのconfig情報を.envファイルへ移行

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJPWi1mgMcErHaLya12DVMn7mY2bEjyyc',
-  authDomain: 'react-chat-app-d75b4.firebaseapp.com',
-  databaseURL: 'https://react-chat-app-d75b4-default-rtdb.firebaseio.com',
-  projectId: 'react-chat-app-d75b4',
-  storageBucket: 'react-chat-app-d75b4.appspot.com',
-  messagingSenderId: '484640258238',
-  appId: '1:484640258238:web:32d675a8655614f41106ef',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
